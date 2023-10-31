7dc2eaac0bce99974e2ec0f6f78fd16023bb446b
GeorgeRG <48743356+BrickBrainz@users.noreply.github.com>
2023-10-30 19:04:48 -0500

    Update Main.java
    a;lksjdf

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -12,7 +17,7 @@ public class Main {
         }
         */
         //String fileName = args[0];
-        File infile = new File("wordsForSpellChecker.txt");
+        File infile = new File("C:\\Users\\gguil\\OneDrive\\Desktop\\spellChecker-main\\wordsForSpellChecker.txt");
 
         try{
             //File infile = new File(fileName);
@@ -20,15 +25,44 @@ public class Main {
 
             while(input.hasNextLine()){
                 String word = input.nextLine();
-                trie.insert(word);
+                String noPuncWord = word.replaceAll("[^\\sa-zA-Z0-9]", "");
+                //System.out.println(noPuncWord);
+                trie.insert(noPuncWord);
             }
-             input.close()
+             input.close();
         } catch (FileNotFoundException e){
             System.out.println(e.getMessage());
         }
+
+        try {
+			ObjectOutputStream o = new ObjectOutputStream(new FileOutputStream(new File("trieTree.ser")));
+
+			o.writeObject(trie);
+
+			o.close();
+
+            /*
+			FileInputStream fi = new FileInputStream(new File("myObjects.txt"));
+			ObjectInputStream oi = new ObjectInputStream(fi);
+
+			// Read objects
+			Person pr1 = (Person) oi.readObject();
+			Person pr2 = (Person) oi.readObject();
+
+			System.out.println(pr1.toString());
+			System.out.println(pr2.toString());
+
+			oi.close();
+			fi.close();
+            */
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		}
         
-       
-            
+        
         //Scanner scanner = new Scanner(System.in);
         //System.out.println("enter a word");
 
