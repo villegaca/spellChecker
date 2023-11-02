3898dbbf00b2931b067d80f816d1c82fc2a75c6f
GeorgeRG <48743356+BrickBrainz@users.noreply.github.com>
2023-11-01 23:54:45 -0500

    Update Trie.java
    changes

--- Trie.java
+++ Trie.java
@@ -5,18 +5,22 @@ public class Trie implements Serializable {
     private Node root;
 
     public Trie(){
-        root = new Node('\0');
+        root = new Node('\0', 0);
     }
 
     public class Node implements Serializable{
         private Node[] children; 
         char c = ' ';
         private boolean lastCharacter;
+        int level;
+        private boolean isExplored;
 
-        public Node(char c){
+        public Node(char c, int levelCount){
             this.c = c;
             children = new Node[ALPHABET_SIZE];
             lastCharacter = false;
+            level = levelCount;
+            isExplored = false;
         }
     }
 
@@ -48,9 +52,11 @@ public class Trie implements Serializable {
             index = c - 'a';
             //System.out.println(index);
             if(curr.children[index] == null){
-                curr.children[index] = new Node(c);
+                curr.children[index] = new Node(c, curr.level + 1);
                 curr = curr.children[index];
-                //System.out.println(curr.c);
+                System.out.println(curr.level);
+            } else {
+                curr = curr.children[index];
             }
         }
         curr.lastCharacter = true;
@@ -64,23 +70,44 @@ public class Trie implements Serializable {
         String searchWorked = String.format("%s = %s%n", word, isWordMsg);
         
         newWord = wordModifier.wordDelimitter(word);
-        System.out.println(newWord);
-        System.out.println(newWord.length());
 
         for (int i = 0; i < newWord.length()-1; i++){
             c = newWord.charAt(i);
             //System.out.println(c);
             index = c - 'a';
             if(curr.children[index] == null){
-                System.out.println(c);
                 return searchFailed;
             }
             curr = curr.children[index];
         }
+
+        ArrayList<String> firstListOfWords = getAllWordsByDistance(newWord, curr, 3);
+
         //might need to work on this
         if(!curr.lastCharacter){
             return searchFailed;
         }
+        System.out.println(curr.level);
         return searchWorked;
     }
+    
+    static ArrayList<String>[] getAllWordsByDistance(String word, Node lastNodeForWord, int distance){
+        Node root = lastNodeForWord;
+        Node curr = lastNodeForWord;
+        ArrayList<String> suggestionList = new ArrayList<String>();
+
+        for (int i = 0; i == 3; i++){
+            String temp = "" + word;
+            for(int j = 0; j == 26; j++){
+                //check levenstein length
+                if(curr.children[j] != null && !(curr.children[j].isExplored) /*&& levent stein length <= 3*/){
+                    curr = curr.children[j];
+                    if(curr.lastCharacter){
+                        //add to arraylist
+                    }
+                }
+            }
+        }
+    }
+    
 }
